4264c8d1252ee2d3d1312265630fc71824c6896a
Kmkc Coast <leatilecoast@gmail.com>
2025-04-12 03:14:34 +0200

    Create upload.jsx:

---
+++ upload.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ArtistUploadForm from '../components/ArtistUploadForm';
+
+const UploadPage = () => {
+  return (
+    <div>
+      <h1>Upload Your Song</h1>
+      <ArtistUploadForm />
+    </div>
+  );
+};
+
+export default UploadPage;
