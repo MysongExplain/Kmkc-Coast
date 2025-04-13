1fff8f35d6cbf68a14ba93ab5377c7c4837346a5
Kmkc Coast <leatilecoast@gmail.com>
2025-04-13 07:32:15 +0200

    Update index.jsx

--- index.jsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>Welcome to KMKC COAST on MysongExplain</h1>
-      <p>This is the fan-facing home page.</p>
-      <a href="/dashboard">Go to Dashboard</a>
-    </div>
-  );
-} 
